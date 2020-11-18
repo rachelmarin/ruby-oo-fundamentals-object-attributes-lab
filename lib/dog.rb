1fb2a77b00748f34ad1e512de3b790d76f348479
rachelmarin <Rachelmarinpal@gmail.com>
2020-11-18 02:44:18 -0500

    commit tree 0cdf3b7f5a7cb4677eebc3e56cdd00498bdd6f40

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+    def name
+      @name
+    end
+  
+    def name=(name = Fido)
+      @name = name
+    end
+  
+    def breed
+      @breed
+    end
+  
+    def breed=(breed = Mastiff)
+      @breed = breed
+    end
+  end
---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+class Person
+
+        def name
+          @name
+        end
+      
+        def name=(name = Beyonce)
+          @name = name
+        end
+      
+        def job
+          @job
+        end
+      
+        def job=(job = singer)
+          @job = job
+        end
+    end
+
+    
